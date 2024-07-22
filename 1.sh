6149b028f45286ebf0b8df440c05930fb0371ee9
abhijitsharma1010 <44794851+abhijitsharma1010@users.noreply.github.com>
2024-07-22 17:48:44 +0530

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 TOKEN="7076683762:AAHVFQFxTPmCLN2d4rLdaSC3dzQQn8EB47A"
-ID="1002149806561"
+ID="633143645"
+#ID="1002149806561"
 URL="https://api.telegram.org/bot$TOKEN/sendMessage"
 curl -s -X POST $URL -d chat_id=$ID -d text="Hello World"
