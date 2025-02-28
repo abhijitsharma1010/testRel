72a0d93b2412264ce03ce9e2885e8cd204113aca
abhijitsharma1010 <44794851+abhijitsharma1010@users.noreply.github.com>
2025-02-28 12:51:21 +0530

    Create 28_02v4.sh

---
+++ 28_02v4.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Static header line
+header="Probe ID;;DNS Query;;Probe Ipv4;;ProbeIpv6;;Probe Prefix v4;;Probe Prefix v6;;Probe ASNv4;;Probe Country;;Anchor;;Probe Coordinates;;Resolver1;;Resolver2;;Resolver3"
+
+# Write the header to the newoutput file
+echo "$header" > newoutput
+
+# Read output.txt and store Probe ID, Query, SERVER1, SERVER2, SERVER3 in an associative array
+declare -A probe_data
+while IFS=, read -r probe_id query server1 server2 server3; do
+    probe_id=$(echo "$probe_id" | awk -F': ' '{print $2}')
+    query=$(echo "$query" | awk -F': ' '{print $2}')
+    server1=$(echo "$server1" | awk -F': ' '{print $2}')
+    server2=$(echo "$server2" | awk -F': ' '{print $2}')
+    server3=$(echo "$server3" | awk -F': ' '{print $2}')
+    probe_data["$probe_id"]="$query,$server1,$server2,$server3"
+done < <(grep 'Probe ID:' output.txt)
+
+# Process output1.txt and replace 'DNS query' with the corresponding Query value
+while IFS= read -r line; do
+    probe_id=$(echo "$line" | awk '{print $1}')
+    if [[ -n "${probe_data[$probe_id]}" ]]; then
+        # Extract Query, SERVER1, SERVER2, SERVER3 from the associative array
+        IFS=, read -r query server1 server2 server3 <<< "${probe_data[$probe_id]}"
+        # Replace 'DNS query' with the Query value
+        updated_line=$(echo "$line" | sed "s/DNS query/$query/")
+        # Append SERVER1, SERVER2, SERVER3 to the end of the line
+        echo "$updated_line,$server1,$server2,$server3" >> newoutput
+    else
+        # If no matching Probe ID, keep the line as is
+        echo "$line" >> newoutput
+    fi
+done < output1.txt
+
+echo "New file 'newoutput' has been created with the updated data and header."
